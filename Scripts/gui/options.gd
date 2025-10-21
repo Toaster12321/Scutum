0811cc4e65f30adb3d8d9514cb26550c311d321c
Toaster12321 <tonsterrocks123@gmail.com>
2025-10-21 16:02:14 -0700

    resolution dropdown
    implemented a resolution menu that can change screen size
    also has a confirmation

--- Scripts/gui/options.gd
+++ Scripts/gui/options.gd
@@ -1,9 +1,31 @@
 extends Control
 
-var master_bus = AudioServer.get_bus_index("Master")
+@onready var res_option_button: OptionButton = $VBoxContainer2/res_OptionButton
+@onready var resolution_confirmation: Control = $"../ResolutionConfirmation"
+@onready var timer: Timer = $"../ResolutionConfirmation/Timer"
+@onready var timer_label: Label = $"../ResolutionConfirmation/TimerLabel"
+@onready var res_yes_button: Button = $"../ResolutionConfirmation/HBoxContainer/ResYesButton"
+@onready var res_no_button: Button = $"../ResolutionConfirmation/HBoxContainer/ResNoButton"
+
+var previous_resolution = {}
+
+var master_bus = AudioServer.get_bus_index("Master") #audio buses
+var sfx_bus = AudioServer.get_bus_index("SFX")
+var music_bus = AudioServer.get_bus_index("Music")
 
 
-func _on_h_slider_value_changed(value: float) -> void:
+func _ready() -> void:
+	res_yes_button.pressed.connect(_on_res_yes_pressed)
+	res_no_button.pressed.connect(_on_res_no_pressed)
+	resolution_confirmation.visible = false
+	add_resolutions() #populate dropdown resolution menu
+
+func _process(delta: float) -> void:
+	timer_label.text = str(int(timer.time_left))
+	pass
+
+
+func _on_h_slider_value_changed(value: float) -> void: #master volume bind
 	AudioServer.set_bus_volume_db( master_bus, value )
 	
 	if value == -30:
@@ -11,3 +33,72 @@ func _on_h_slider_value_changed(value: float) -> void:
 	else:
 		AudioServer.set_bus_mute( master_bus, false)
 	pass
+
+
+func _on_sfx_slider_value_changed(value: float) -> void: #sfx volume bind
+	AudioServer.set_bus_volume_db( sfx_bus, value )
+	
+	if value == -30:
+		AudioServer.set_bus_mute( sfx_bus, true)
+	else:
+		AudioServer.set_bus_mute( sfx_bus, false)
+	pass # Replace with function body.
+
+
+
+func _on_music_slider_value_changed(value: float) -> void: #music volume bind
+	AudioServer.set_bus_volume_db( music_bus, value )
+	
+	if value == -30:
+		AudioServer.set_bus_mute( music_bus, true)
+	else:
+		AudioServer.set_bus_mute( music_bus, false)
+	pass # Replace with function body.
+
+
+func add_resolutions() -> void:
+	for i in PauseMenu.resolutions: #populates dropdown resolution menu
+		res_option_button.add_item(i)
+
+
+func _on_res_option_button_item_selected(index: int) -> void: #
+	var key = res_option_button.get_item_text(index) #key is the index of selected item in the dropdown menu
+	print(previous_resolution)
+	get_window().set_size(PauseMenu.resolutions[key])  #set window size to the key
+	PauseMenu.center_window() #center the window so it doesnt move to a weird position
+	show_resolution_confirmation()
+	pass # Replace with function body.
+
+
+func update_button_values():
+	var window_size_string = str(get_window().size.x,"x",get_window().size.y) #gets x and y of the window
+	var resolutions_index = PauseMenu.resolutions.keys().find(window_size_string) #find the corresponding string in the array from the key
+	previous_resolution = resolutions_index
+	res_option_button.selected = resolutions_index #set index to the selected button
+
+
+func show_resolution_confirmation() -> void:
+	resolution_confirmation.visible = true
+	timer.start()
+	pass
+
+
+func _on_res_yes_pressed() -> void:
+	resolution_confirmation.visible = false
+	timer.stop()
+	pass
+
+
+func _on_res_no_pressed() -> void:
+	timer.stop()
+	var key = res_option_button.get_item_text(previous_resolution) #key is the index of previous resolution
+	resolution_confirmation.visible = false #tunr off UI
+	get_window().set_size(PauseMenu.resolutions[key])  #set window size to the key
+	PauseMenu.center_window() #center the window so it doesnt move to a weird position
+	res_option_button.selected = previous_resolution #reselect old options
+	pass
+
+
+func _on_timer_timeout() -> void:
+	_on_res_no_pressed() #performs the same as a no press
+	pass # Replace with function body.
--- Scripts/gui/pause_menu.gd
+++ Scripts/gui/pause_menu.gd
@@ -3,6 +3,18 @@ extends CanvasLayer
 signal shown
 signal hidden
 
+var resolutions = {
+	"3840x2160": Vector2i(3840,2160),
+	"2560x1440": Vector2i(2560,1440),
+	"1920x1080": Vector2i(1920,1080),
+	"1366x768": Vector2i(1366,768),
+	"1280x720": Vector2i(1280,720),
+	"1440x900": Vector2i(1440,900),
+	"1600x900": Vector2i(1600,900),
+	"1024x600": Vector2i(1024,600),
+	"800x600": Vector2i(800,600)
+}
+
 @onready var audio_stream_player: AudioStreamPlayer = $Control/AudioStreamPlayer
 @onready var button_resume: Button = $Control/Pause/PauseButtons/Button_Resume
 @onready var button_options: Button = $Control/Pause/PauseButtons/Button_Options
@@ -32,6 +44,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		if is_paused == false:
 			show_pause_menu()
+			var options_menu = get_node("Control/Options")
+			options_menu.update_button_values()
 		else:
 			hide_pause_menu()
 		get_viewport().set_input_as_handled() #set the viewport event as handled tpo not be processed by other nodes
@@ -87,3 +101,9 @@ func _reset_menu() -> void:
 	options_menu.visible = false
 	quit_confirmation.visible = false
 	pass
+
+
+func center_window():
+	var screen_center = DisplayServer.screen_get_position() + DisplayServer.screen_get_size() / 2
+	var window_size = get_window().get_size_with_decorations()
+	get_window().set_position(screen_center - window_size /2)
