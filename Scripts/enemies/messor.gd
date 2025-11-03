caefe02de58c09686e690438892c3dcaa4b95f23
Toaster12321 <tonsterrocks123@gmail.com>
2025-11-02 16:35:21 -0800

    Messor
    still working on messor
    fixed some animations and attack monitoring
    messor hitboxes can now flip with him and work properly
    starting to work on a summon state

--- Scripts/enemies/messor.gd
+++ Scripts/enemies/messor.gd
@@ -11,17 +11,22 @@ var hp : int = 10
 var direction : Vector2 = Vector2.ZERO
 var facing_direction : float = 1
 var finding_player : bool = false
+var player_seen : bool = false
+var attack_select : int
 
-@onready var hitbox: Hitbox = $BossNode/Hitbox
-@onready var hurtbox: Hurtbox = $BossNode/Hurtbox
-@onready var boss_animation_player: AnimationPlayer = $BossNode/BossAnimationPlayer
-@onready var boss_effect_animation_player: AnimationPlayer = $BossNode/BossEffectAnimationPlayer
+@onready var hitbox: Hitbox = $Hitbox
+@onready var hurtbox: Hurtbox = $Hurtbox
+@onready var boss_animation_player: AnimationPlayer = $BossAnimationPlayer
+@onready var boss_effect_animation_player: AnimationPlayer = $BossEffectAnimationPlayer
 @onready var sprite: Node2D = $BossNode
+@onready var cast_shadow: ClassShadow = $BossNode/BossSprite/CastShadow
 @onready var vision_area: VisionArea = $BossNode/VisionArea
 
 
 func _ready() -> void:
+	set_direction( global_position.direction_to(GlobalPlayerManager.knight.global_position) )
 	vision_area.player_enetered.connect( _on_knight_entered )
+	vision_area.player_exited.connect( _on_knight_exited )
 	
 	hp = max_hp #set boss hp to full
 	
@@ -32,6 +37,7 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+	attack_select = randi_range(0,2)
 	velocity.y += 980 * delta 
 	move_and_slide()
 	
@@ -41,6 +47,7 @@ func _process(delta: float) -> void:
 
 
 func _on_damage_taken( _hurtbox : Hurtbox ) ->  void:
+	set_direction( global_position.direction_to(GlobalPlayerManager.knight.global_position) )
 	if boss_effect_animation_player.current_animation == "flash" or _hurtbox.damage == 0: #gives i frames to boss when hit
 		return
 	hp = clampi( hp - _hurtbox.damage, 0, max_hp) #clamp hp so hp cant fall lower than 0
@@ -54,6 +61,7 @@ func _on_damage_taken( _hurtbox : Hurtbox ) ->  void:
 
 
 func boss_defeated() -> void:
+	cast_shadow.queue_free()
 	boss_animation_player.play("death")
 	set_hit_boxes(false)
 	
@@ -70,26 +78,57 @@ func set_hit_boxes( _bool : bool = true ) -> void:
 
 
 func find_player() -> void:
+	if player_seen == true:
+		_on_knight_entered()
+		return
+		
+	set_direction( global_position.direction_to(GlobalPlayerManager.knight.global_position) )
 	boss_animation_player.play("idle")
 	finding_player = true
 	pass
 
 
 func set_direction( _new_direction : Vector2 ) -> void:
-	if _new_direction != Vector2.ZERO: #if we arent idle set direction to passed in
+	if _new_direction != Vector2.ZERO: 
 		direction = _new_direction
 	
-		if direction.x < 0: #since sprite starts left side we flip logic
-			sprite.scale.x = 1   #if we are facing left flip to right
-			facing_direction = -1
-		elif direction.x > 0: 
-			sprite.scale.x = -1 #if we are facing right flip to left
+		if direction.x < 0: 
+			sprite.scale.x = -1 #if we are facing right flip left
 			facing_direction = 1
+		elif direction.x > 0: 
+			sprite.scale.x = 1 #if we are facing left flip right
+			facing_direction = -1
 	
 	direction_changed.emit( direction ) #emit signal for direction changed to switch any nodes to new direction
 
 
 func _on_knight_entered() -> void:
+	player_seen = true
 	finding_player = false
 	velocity = Vector2.ZERO
+	
+	match attack_select:
+		0:
+			boss_animation_player.play("double_attack_" + anim_direction())
+		1:
+			boss_animation_player.play("single_attack_" + anim_direction())
+		2:
+			boss_animation_player.play("skill")
+	
+	await boss_animation_player.animation_finished
+	boss_animation_player.play("idle2")
+	await get_tree().create_timer(0.8).timeout
+	
+	find_player()
 	pass
+
+
+func _on_knight_exited():
+	player_seen = false
+
+
+func anim_direction() -> String: #returns a left or right based on the current direction of the player
+	if direction.x < 0:
+		return "left"
+	else:
+		return "right"
