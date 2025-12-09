2b417e13257328426e77c458aaa464fc4d42a79b
Toaster12321 <tonsterrocks123@gmail.com>
2025-12-08 18:01:33 -0800

    Knight Shield Bug Fix
    fixed bug where knight shield deflect wasnt working if there was an overlapping hurtbox
    added a point light 2D to starting area
    added light source to knight scene

--- Scripts/Knight/shieldbox.gd
+++ Scripts/Knight/shieldbox.gd
@@ -4,18 +4,22 @@ class_name Shieldbox extends Area2D
 signal deflected( hurt_box : Hurtbox )
 
 func shield_damage( hurt_box : Hurtbox ) -> bool: #function to shield damage based off passed hurtbox scene
-	var owner = get_parent() #owner is the knight
-	if owner == null:
+	var shield_owner = get_parent() #owner is the knight
+	if shield_owner == null:
 		return false
 	
-	var attacker_dir = (hurt_box.global_position - owner.global_position) #which horizontal side the attack came from relative to the knight
-	var attack_side = int(sign(attacker_dir.x)) #-1 left, 1 right, 0 if alinged
+	var hurtbox_owner = hurt_box.get_parent() #hurtbox assigned to enemy
+	if shield_owner == null:
+		return false
 	
-	var owner_facing_dir = GlobalPlayerManager.knight.facing_direction #-1 left, 1 right
+	var attacker_dir = (hurtbox_owner.global_position - shield_owner.global_position) #which horizontal side the attack came from relative to the knight
+	var attack_side = int(sign(attacker_dir.x)) #-1 left, 1 right, 0 if aligned
 	
-	if attack_side == owner_facing_dir: #if attack side equals facing direction attack come front -> block
+	var shield_owner_facing_dir = GlobalPlayerManager.knight.facing_direction #-1 left, 1 right
+	
+	if attack_side == shield_owner_facing_dir: #if attack side equals facing direction attack come front -> block
 		deflected.emit(hurt_box)
 		return true
 	else: #take damage shielding in wrong direction
-		owner._take_damage(hurt_box)
+		shield_owner._take_damage(hurt_box)
 		return false
