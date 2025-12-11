23881ecb2d36669980b7be0891ead2c307786d90
Toaster12321 <tonsterrocks123@gmail.com>
2025-12-10 16:57:43 -0800

    Small Bug Fixes

--- Scripts/states/enemy states/enemy_state_patrol.gd
+++ Scripts/states/enemy states/enemy_state_patrol.gd
@@ -62,7 +62,7 @@ func exit() -> void:
 
 func process( _delta : float ) -> EnemyState:
 	if patrol_walk == true:
-		if enemy.global_position.distance_to( target.target_position ) < 4: #if the enemy is within 4 pixels of the target position, idle
+		if enemy.global_position.distance_to( target.target_position ) < 10: #if the enemy is within 10 pixels of the target position, idle
 			idling()
 		if enemy.global_position.distance_to( spawn_position ) > enemy.patrol_range:#if the enemy is out of range, change state to wander
 			return wander
